3125fe7bf81356bed4a99f54c9449d8c725aba1e
Andrew <J84morgan@gmail.com>
2019-09-22 11:33:01 +0300

    push to github

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const router = express.Router();
+const { userSignupValidator } = require('../validator');
+
+const { signup, signin, signout, requireSignin } = require('../controllers/auth');
+
+router.post('/signup', userSignupValidator, signup);
+router.post('/signin', signin);
+router.post('/signout', signout);
+
+module.exports = router;
